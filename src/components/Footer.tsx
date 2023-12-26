9e76811173f6b9c61bd21fbcb7f93843da9f7d21
Albin Lamichhane <albin.lamichhane@tuni.fi>
2023-12-26 21:43:09 +0200

    added footer fix:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-[rgb(11,134,139)]   dark:bg-gray-800  bottom-0 w-full py-4">
       <p className="font-[400] lg:text-xl sm:text-sm  text-center  text-white">
-        Images are From Flickr
+        Images are From Flickr.
       </p>
     </footer>
   );
